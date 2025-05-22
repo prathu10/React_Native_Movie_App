1b2e3334b54d0803eb695d23fc623bc889c46dfe
Prathu <prathamesh4402@gmail.com>
2025-05-22 02:09:18 -0600

    Add new logo, SearchBar component, and UI adjustments
    Replaced app assets with a new logo and updated app name to "MovieFlex." Introduced a reusable SearchBar component to improve UX. Adjusted TabIcon styling for better visual consistency.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ import { Text } from 'react-native';
 const TabIcon = ({focused, icon, title}: any) => {
     if (focused) {
     return (
-        <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
+        <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden">
 
             <Image source={icon} tintColor="#151312" className="size-5" />
 
--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -2,11 +2,13 @@ import home from "./home.png";
 import search from "./search.png";
 import save from "./save.png";
 import person from "./person.png";
+import logo from "./logo.png";
 
 export const icons = {
     home,
     search,
     save,
     person,
+    logo,
 
 };
