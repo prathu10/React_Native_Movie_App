c24d2806e17b4a952cb97ce91dd27d643c081b2d
Prathu <prathamesh4402@gmail.com>
2025-05-27 17:06:17 -0600

    Add movie fetch logic and reusable fetch hook
    Refactored `fetchMovies` to include dynamic query-based API endpoints with proper error handling. Introduced a generic `useFetch` hook to simplify API requests and manage loading, error, and data states in components.

--- services/api.ts
+++ services/api.ts
@@ -9,6 +9,22 @@ export const TMDB_CONFIG ={
 
 export const fetchMovies = async ( { query } :{ query: string}) => {
 
+    const endpoint = query
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        :`${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`; //this will give the most popular MOVIES
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if(!response.ok){
+        // @ts-ignore
+        throw new Error('Something went wrong', response.statusText);
+    }
+
+    const data = await response.json();
+    return data.results;
+
 }
 
 
---
+++ services/useFetch.ts
@@ -0,0 +1,39 @@
+//this hook will allow us to handle api requests without cluttering our components
+// useState lets you store and update values (like data, loading state, error).
+// useEffect lets you run something after the component renders, like making an API call.
+
+import {useState, useEffect} from "react";
+
+const useFetch =<T> (fetchFunction: () => Promise<T>, autoFetch = true) => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            setError(null);
+            const result = await fetchFunction();
+            setData(result);
+        } catch (err) {
+            setError(err instanceof Error ? err : new Error('An error occurred'));
+        } finally {
+            setLoading(false);
+        }
+    }
+    const reset = () => {
+        setData(null);
+        setLoading(false);
+        setError(null);
+    }
+
+    useEffect(() => {
+        if(autoFetch){
+            fetchData();
+        }
+
+    }, []);
+    return {data, loading, error, refetch: fetchData, reset};
+};
+
+export default useFetch;
