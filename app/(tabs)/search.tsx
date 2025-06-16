6beb2839bfcfa0976a5f5d515f0ac61553333a85
Prathu <prathamesh4402@gmail.com>
2025-06-16 14:50:58 -0600

    Add `getTrendingMovies` to fetch top movies and improve `updateSearchCount` handling logic

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -23,11 +23,6 @@ const Search = () => {
         const timeoutId =setTimeout(async () => {
             if (searchQuery.trim()) {
                 await loadMovies();
-
-                if(movies?.length > 0 && movies?.[0])
-                    await updateSearchCount(searchQuery, movies[0]);
-
-
             } else {
                 reset();
             }
@@ -35,6 +30,12 @@ const Search = () => {
         return () => clearTimeout(timeoutId);
 
     }, [searchQuery]);
+
+    useEffect(() => {
+        if(movies?.length > 0 && movies?.[0]) {
+            updateSearchCount(searchQuery, movies[0]);
+        }
+    }, [movies]);
 
 
     return (
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -4,7 +4,6 @@ import { ID } from "react-native-appwrite";
 //track the searches made by a user
 
 
-
 const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
@@ -58,4 +57,21 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     //if a document is found increment the searchCount field
     //if no document is found
         //create a new document in Appwrite database -> 1
+}
+
+
+//create a function to get trending movies
+
+export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> => {
+    try{
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc('count'),
+        ])
+
+        return result.documents as unknown as TrendingMovie[];
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
 }
