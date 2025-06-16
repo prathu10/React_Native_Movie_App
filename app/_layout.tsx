32d41ff91024b2c3b4601c984d4fc9b83a23369e
Prathu <prathamesh4402@gmail.com>
2025-06-16 15:51:24 -0600

    Add `StatusBar` to hide the status bar in `RootLayout`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { Stack } from "expo-router";
 import './globals.css';
+import {StatusBar} from "react-native";
 
 export default function RootLayout() {
-  return <Stack>
+  return (
+      <>
+        <StatusBar hidden={true} />
+      <Stack>
+
     <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
 
     <Stack.Screen name="movies/[id]" options={{headerShown: false}}/>
-  </Stack>;
+  </Stack>
+      </>
+  );
 }
